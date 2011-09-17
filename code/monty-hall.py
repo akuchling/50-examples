f48b5eafc1b67bba6a7efb8011b2c815ed231f19
Andrew M. Kuchling <amk@amk.ca>
2011-09-17 18:44:27 -0400

    Add --verbose switch to print the results of each trial

--- code/monty-hall.py
+++ code/monty-hall.py
@@ -6,38 +6,66 @@
 
 import argparse, random
 
-def simulate(num_doors, switch):
+def simulate(num_doors, switch, verbose):
     """(int, bool): bool
 
     Carry out the game for one contestant.  If 'switch' is True,
     the contestant will switch their chosen door when offered the chance.
+    Returns a Boolean value telling whether the simulated contestant won.
     """
 
     # Doors are numbered from 0 up to num_doors-1 (inclusive).
 
-    # Randomly choose the door 
+    # Randomly choose the door hiding the prize.
     winning_door = random.randint(0, num_doors-1)
-    
+    if verbose:
+        print('Prize is behind door {0}'.format(winning_door+1))
+
     # The contestant picks a random door, too.
     choice = random.randint(0, num_doors-1)
+    if verbose:
+        print('Contestant chooses door {0}'.format(choice+1))
 
-    # Pick a list of all but two doors to open.
-    available_doors = list(range(num_doors))
-    while len(available_doors) > 2:
-        door_to_remove = random.choice(available_doors)
-        if door_to_remove in (winning_door, choice):
+    # The host opens all but two doors.  The host
+    closed_doors = list(range(num_doors))
+    while len(closed_doors) > 2:
+        # Randomly choose a door to open.
+        door_to_remove = random.choice(closed_doors)
+
+        # The host will never open the winning door, or the door
+        # chosen by the contestant.
+        if door_to_remove == winning_door or door_to_remove == choice:
             continue
-        available_doors.remove(door_to_remove)
+
+        # Remove the door from the list of closed doors.
+        closed_doors.remove(door_to_remove)
+        if verbose:
+            print('Host opens door {0}'.format(door_to_remove+1))
 
     # Does the contestant want to switch their choice?
     if switch:
+        if verbose:
+            print('Contestant switches from door {0} '.format(choice+1), end='')
+
+        # There are two closed doors left.  The contestant will never
+        # choose the same door, so we'll remove that door as a choice.
+        available_doors = list(closed_doors) # Make a copy of the list.
         available_doors.remove(choice)
-        
         assert len(available_doors) == 1
+
+        # Change choice to the only door available.
         choice = available_doors.pop()
+        if verbose:
+            print('to {0}'.format(choice+1))
 
     # Did the contestant win?
-    return (choice == winning_door)
+    won = (choice == winning_door)
+    if verbose:
+        if won:
+            print('Contestant WON', end='\n\n')
+        else:
+            print('Contestant LOST', end='\n\n')
+    return won
 
 
 def main():
@@ -48,31 +76,33 @@ def main():
                         help='number of doors offered to the contestant')
     parser.add_argument('--trials', default=10000, type=int, metavar='int',
                         help='number of trials to perform')
+    parser.add_argument('--verbose', default=False, action='store_true',
+                        help='display the results of each trial')
     args = parser.parse_args()
-    
-    print('Simulating {0} contestants...'.format(args.trials))
+
+    print('Simulating {0} trials...'.format(args.trials))
 
     # Carry out the trials
     winning_non_switchers = 0
     winning_switchers = 0
     for i in range(args.trials):
         # First, do a trial where the contestant never switches.
-        won = simulate(args.doors, switch=False)
+        won = simulate(args.doors, switch=False, verbose=args.verbose)
         if won:
             winning_non_switchers += 1
-            
+
         # Next, try one where the contestant switches.
-        won = simulate(args.doors, switch=True)
+        won = simulate(args.doors, switch=True, verbose=args.verbose)
         if won:
             winning_switchers += 1
-        
+
     print('    Switching won {0:5} times out of {1} ({2}%)'.format(
             winning_switchers, args.trials,
             (winning_switchers / args.trials * 100 ) ))
     print('Not switching won {0:5} times out of {1} ({2}%)'.format(
             winning_non_switchers, args.trials,
             (winning_non_switchers / args.trials * 100 ) ))
-            
-    
+
+
 if __name__ == '__main__':
     main()
