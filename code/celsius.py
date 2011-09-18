2883543418de11f27df30e653ae78ed86f4e54ac
Andrew M. Kuchling <amk@amk.ca>
2011-09-18 09:04:59 -0400

    Add celsius converter

---
+++ code/celsius.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+
+def convert_f2c(S):
+    """(str): float
+
+    Converts a Fahrenheit temperature represented as a string
+    to a Celsius temperature.
+    """
+    fahrenheit = float(S)
+    celsius = (fahrenheit - 32) *5/9
+    return celsius
+
+def main():
+    # If no arguments were given, print a helpful message
+    if len(sys.argv) == 1:
+        print('Usage: {0} temp1 temp2 ...'.format(sys.argv[0]))
+        sys.exit(0)
+
+    # Loop over the arguments
+    for arg in sys.argv[1:]:
+        try:
+            celsius = convert_f2c(arg)
+        except ValueError:
+            print("{0!r} is not a numeric value".format(arg),
+                  file=sys.stderr)
+        else:
+            print('{0}\260F = {1:g}\260C'.format(arg, 
+                                                 round(celsius, 0)))
+
+if __name__ == '__main__':
+    main()
